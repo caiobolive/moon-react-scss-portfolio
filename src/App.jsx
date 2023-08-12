eab17f46f5722408a6816ffa96f9a21442907b3f
caiobolive <caiobolive@gmail.com>
2023-08-12 11:54:57 -0300

    adding color pallete variables and background video

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import MainContent from "./components/MainContent";
 import NavBar from "./components/NavBar";
+import moonWindowClip from "/video/moon_window_clip.mp4"
 
 import "./styles/components/app.scss";
 
 function App() {
   return (
     <div id="portfolio">
+      <video id="moon-window-background" src={moonWindowClip} autoPlay muted loop/>
       <NavBar />
       <MainContent />
     </div>
