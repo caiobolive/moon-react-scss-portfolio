e06c4e9f417f6a503583a166d6b7f9641f7176cf
caiobolive <caiobolive@gmail.com>
2023-08-17 18:02:17 -0300

    navbar alignment and styling improvements

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -14,23 +14,20 @@ const NavBar = () => {
     <nav id="navbar" className={click ? "navbar active" : "navbar"}>
       <div className="navbar__container">
         <div className="navbar__container__title">
-          <h1 className="navbar__container__title__text neon-text">CAIO OLIVEIRA</h1>
+          <Link to="/" className="navbar__container__title__text neon-text">CAIO OLIVEIRA</Link>
         </div>
         <ul className={click ? "navbar__container__list active" : "navbar__container__list"}>
           <li className={click ? "navbar__container__list__item active" : "navbar__container__list__item"} style={{ '--transition-delay': '100ms' }}>
-            <Link to="/" className="navbar__container__list__item__link" onClick={handleClick}>Intro</Link>
+            <Link to="/about" className="navbar__container__list__item__link neon-text" onClick={handleClick}>About</Link>
           </li>
           <li className={click ? "navbar__container__list__item active" : "navbar__container__list__item"} style={{ '--transition-delay': '200ms' }}>
-            <Link to="/about" className="navbar__container__list__item__link" onClick={handleClick}>About</Link>
+            <Link to="/skills" className="navbar__container__list__item__link neon-text" onClick={handleClick}>Skills</Link>
           </li>
           <li className={click ? "navbar__container__list__item active" : "navbar__container__list__item"} style={{ '--transition-delay': '300ms' }}>
-            <Link to="/skills" className="navbar__container__list__item__link" onClick={handleClick}>Skills</Link>
+            <Link to="/projects" className="navbar__container__list__item__link neon-text" onClick={handleClick}>Projects</Link>
           </li>
           <li className={click ? "navbar__container__list__item active" : "navbar__container__list__item"} style={{ '--transition-delay': '400ms' }}>
-            <Link to="/projects" className="navbar__container__list__item__link" onClick={handleClick}>Projects</Link>
-          </li>
-          <li className={click ? "navbar__container__list__item active" : "navbar__container__list__item"} style={{ '--transition-delay': '500ms' }}>
-            <Link to="/experiences" className="navbar__container__list__item__link" onClick={handleClick}>Experiences</Link>
+            <Link to="/experiences" className="navbar__container__list__item__link neon-text" onClick={handleClick}>Experiences</Link>
           </li>
         </ul>
         <IconButton className="navbar__container__icon" onClick={handleClick}>
