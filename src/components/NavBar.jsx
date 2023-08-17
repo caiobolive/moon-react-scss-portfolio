633359d3b93dcb215582df786d72ed1ebdf3e886
caiobolive <caiobolive@gmail.com>
2023-08-16 23:44:54 -0300

    updating responsive glassmorphism navbar with styling and animation

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,30 +1,43 @@
 import "../styles/components/navbar.scss";
 import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { IconButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
 const NavBar = () => {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
   return (
-    <header id="navbar" className="navbar">
-      <div className="navbar__title">
-        <h1 className="navbar__title__text neon-text">CAIO OLIVEIRA</h1>
+    <nav id="navbar" className={click ? "navbar active" : "navbar"}>
+      <div className="navbar__container">
+        <div className="navbar__container__title">
+          <h1 className="navbar__container__title__text neon-text">CAIO OLIVEIRA</h1>
+        </div>
+        <ul className={click ? "navbar__container__list active" : "navbar__container__list"}>
+          <li className={click ? "navbar__container__list__item active" : "navbar__container__list__item"} style={{ '--transition-delay': '100ms' }}>
+            <Link to="/" className="navbar__container__list__item__link" onClick={handleClick}>Intro</Link>
+          </li>
+          <li className={click ? "navbar__container__list__item active" : "navbar__container__list__item"} style={{ '--transition-delay': '200ms' }}>
+            <Link to="/about" className="navbar__container__list__item__link" onClick={handleClick}>About</Link>
+          </li>
+          <li className={click ? "navbar__container__list__item active" : "navbar__container__list__item"} style={{ '--transition-delay': '300ms' }}>
+            <Link to="/skills" className="navbar__container__list__item__link" onClick={handleClick}>Skills</Link>
+          </li>
+          <li className={click ? "navbar__container__list__item active" : "navbar__container__list__item"} style={{ '--transition-delay': '400ms' }}>
+            <Link to="/projects" className="navbar__container__list__item__link" onClick={handleClick}>Projects</Link>
+          </li>
+          <li className={click ? "navbar__container__list__item active" : "navbar__container__list__item"} style={{ '--transition-delay': '500ms' }}>
+            <Link to="/experiences" className="navbar__container__list__item__link" onClick={handleClick}>Experiences</Link>
+          </li>
+        </ul>
+        <IconButton className="navbar__container__icon" onClick={handleClick}>
+          {click ? <CloseIcon /> : <MenuIcon />}
+        </IconButton>
       </div>
-      <ul className="navbar__list">
-        <li className="navbar__list__item">
-          <Link to="/" className="navbar__list__item__link">Intro</Link>
-        </li>
-        <li className="navbar__list__item">
-          <Link to="/about" className="navbar__list__item__link">About</Link>
-        </li>
-        <li className="navbar__list__item">
-          <Link to="/skills" className="navbar__list__item__link">Skills</Link>
-        </li>
-        <li className="navbar__list__item">
-          <Link to="/projects" className="navbar__list__item__link">Projects</Link>
-        </li>
-        <li className="navbar__list__item">
-          <Link to="/experiences" className="navbar__list__item__link">Experiences</Link>
-        </li>
-      </ul>
-    </header>
+    </nav>
   );
 };
 
