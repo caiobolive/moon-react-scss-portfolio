35206b820a3ed348a789f749c40d1ba0b0cc81e1
caiobolive <caiobdo@gmail.com>
2025-08-13 09:44:04 -0300

    Update moon window parallax: replace PNG with JPG image and update related styles

--- src/components/MoonWindowParallax.jsx
+++ src/components/MoonWindowParallax.jsx
@@ -2,35 +2,35 @@ import React, { Component } from 'react';
 import { MouseParallax } from "react-just-parallax";
 import "../styles/components/moonwindowparallax.scss";
 import moonWindowFrame from "/img/moon_window_parallax/moon_window_frame.png";
-import moonWindowProfile from "/img/moon_window_parallax/moon_window_profile.png";
+import moonWindowProfile from "/img/moon_window_parallax/moon_window_profile.jpg";
 import moonWindowReflex from "/video/moon_window_reflex_VP8.webm";
 
-class MoonWindowParallax extends Component {  
+class MoonWindowParallax extends Component {
   render() {
     return (
       <div id="moon-window-parallax" className="moon-window-parallax">
         <MouseParallax isAbsolutelyPositioned="true" strength="-0.05" enableOnTouchDevice="true">
-          <img 
-            id="moon-window-profile" 
-            className="moon-window-parallax__window-profile parallax" 
+          <img
+            id="moon-window-profile"
+            className="moon-window-parallax__window-profile parallax"
             src={moonWindowProfile}
           />
         </MouseParallax>
         <MouseParallax isAbsolutelyPositioned="true" strength="-0.08" enableOnTouchDevice="true">
-        <video 
-            controls 
-            muted 
-            loop 
-            autoPlay={true} 
-            id="moon-window-reflex" 
+        <video
+            controls
+            muted
+            loop
+            autoPlay={true}
+            id="moon-window-reflex"
             className="moon-window-parallax__window-reflex parallax" >
             <source src={moonWindowReflex} type="video/webm" />
           </video>
         </MouseParallax>
         <MouseParallax isAbsolutelyPositioned="true" strength="-0.08" enableOnTouchDevice="true">
-          <img 
-            id="moon-window-frame" 
-            className="moon-window-parallax__window-frame parallax" 
+          <img
+            id="moon-window-frame"
+            className="moon-window-parallax__window-frame parallax"
             src={moonWindowFrame}
           />
         </MouseParallax>
